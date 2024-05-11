2a1906d2d107b1d907700af15a6949f5ddf226a0
LesPaul997 <mariogiordano941@gmail.com>
2024-05-11 21:28:22 +0200

    add : file js and form

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
 'use strict';
 
+//ELEMENTI DELLA TRACCIA
+const formCalculator = document.getElementById('calculator-form');
+const ticketPriceForKm = 0.21;
+const percentageUnder = 20;
+const percentageOver = 40;
+
+//ELEMENTI DATI DALL'UTENTE
+const userKm = document.getElementById('user-km');
+const userAge = document.getElementById('user-age');
+
+
+
+//PREZZO DEL BIGLIETTO SENZA SCONTO
+let ticketprice = ticketPriceForKm * userAge;
+
+//RISULTATO
+const result = document.getElementById('result');
+const resultContainer = document.getElementById('result-container');
+
+ if (userAge < 18) {
+    ticketprice = ticketprice - ((ticketprice * percentageUnder) / 100)
+} else if (userAge > 65) {
+    ticketprice = ticketprice - ((ticketprice * percentageOver) / 100)
+} 
+
+
+formCalculator.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+  
+    //inserisco risultato nel dom
+    result.innerText = ticketprice;
+    //visualizzo contenitore risultato
+    resultContainer.classList.remove('d-none');
+  });
+
