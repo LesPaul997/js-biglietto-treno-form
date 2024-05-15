19d6c23800f4ea616bdc00fbe2beee1f8958a28e
LesPaul997 <mariogiordano941@gmail.com>
2024-05-15 07:04:17 +0200

    end project

--- js/main.js
+++ js/main.js
@@ -23,36 +23,50 @@
 
 //////////////////////////////////////////////////////
 
-//ELEMENTI DELLA TRACCIA
+//ELEMENTI DELLA PAGINA
 const formCalculator = document.getElementById('calculator-form'); 
+const resultElement = document.getElementById('result');
+const kmField = document.getElementById('user-km');
+const ageField = document.getElementById('user-age');
+
+
+
+//ELEMENTI DELLA TRACCIA
 const ticketPriceForKm = 0.21;
 const percentageUnder = 20;
 const percentageOver = 40;
-const result = document.getElementById('result');
-const userKm = document.getElementById('user-km').value;
-const userAge = document.getElementById('user-age').value;
-//ELEMENTI DATI DALL'UTENTE
-/* const userKm = parseInt(document.getElementById('user-km')).value;
-const userAge = parseFloat(document.getElementById('user-age')).value;  */
 
-//INIZIO DEL FORM
+
+
+
+// COSE DA FARE ALL'INVIO DEL FORM
 formCalculator.addEventListener('submit', function (event) {
+    // Impedisco il  ricaricamento della pagina
     event.preventDefault();
+    
+// Raccolgo i dati nel form
+const kms = parseInt(kmField.value);
+const age = parseInt(ageField.value);  
 
-    let ticketPrice = ticketPriceForKm * userKm;
-
-//PREZZO DEL BIGLIETTO SCONTO
-/* if (!isNaN(userKm) && !isNaN(userAge)) {
-    let ticketPrice = ticketPriceForKm * userKm; */
-
-if (userAge < 18) {
-    ticketPrice * 0.8; //QUI APPLICO IL 20%
-} else if (userAge > 65) {
-    ticketPrice * 0.6; //QUI APPLICO IL 40%
-} else {
-    ticketPrice;
-}
-
-document.getElementById('result').innerContent = `Il prezzo del tuo biglietto è: €${ticketPrice.toFixed(2)}`; 
-
+    //VALIDATION 💥
+    // Se qualcosa non va, non proseguo
+    if (isNaN(kms) || isNaN(age) || kms <= 0 || age <= 0) {
+            resultElement.innerText = 'sono stati inseriti dati non validi';
+            resultElement.classList.add('text-danger');
+            return;
+    }
+        // Calcolo il prezzo base
+        let ticketPrice = ticketPriceForKm * kms;
+        
+        
+        if (age < 18) {
+            ticketPrice = ticketPrice * 0.8; //QUI APPLICO IL 20%
+        } else if (age > 65) {
+            ticketPrice = ticketPrice * 0.6; //QUI APPLICO IL 40%
+        } 
+        
+        resultElement.classList.remove('text-danger');
+        resultElement.innerText = `Il prezzo del tuo biglietto è: €${ticketPrice.toFixed(2)}`; 
+        console.log(resultElement, resultElement.innerText);
+        
 });
